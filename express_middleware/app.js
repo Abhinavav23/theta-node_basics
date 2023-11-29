d2c17184fcf19a5f69593c1d3c310979ddfcdaa5
Abhinav <abhinav@AVs-Mac.local>
2023-11-29 23:08:12 +0530

    [23 nov 2023] crud operations, get, put, post, delte methods

--- express_middleware/app.js
+++ express_middleware/app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const { genericMiddleware, checkLoginStatus } = require("./midlewares");
 
+app.use(express.json())
+
 // it is a way to define a middleware that needs to be used
 // app.use(genericMiddleware);
 
+let products = [
+  { id: "1001", name: "shirt", price: 2000, color: "white" },
+  { id: "1002", name: "saree", price: 3000, color: "black" },
+];
+
+/*
 app.get("/home", (req, res) => {
   console.log("inside home");
   res.send("home page");
@@ -26,5 +34,67 @@ app.get("/orders", (req, res) => {
   console.log("order handler");
   res.json("my orders");
 });
+*/
+
+
+// ----------------------
+// CRUD operations
+// Read
+app.get("/all", (req, res) => {
+  res.json({ total: products.length, records: products });
+});
+
+// get a product using id
+app.get("/product/:id", (req, res) =>{
+  // to do---
+  res.send("product");
+})
+
+// create
+app.post("/product", (req, res) => {
+  // data using which you will create a product
+  console.log("body", req.body);
+  const id = Math.floor(Math.random()*10000);
+  const newProduct = {id, ...req.body};
+  products.push(newProduct);
+  res.send({total: products.length, message: "created successfully"});
+})
+
+// update
+app.patch("/product/:id", (req, res) => {
+  console.log("body", req.body);
+  console.log("id", req.params.id);
+  products.forEach((product) => {
+    if(product.id === req.params.id){
+      product.price =  req.body.price
+      // product.name =  req.body.name
+      // product = {...product, ...req.body}
+    }
+  })
+  res.send("updated");
+})
+
+// put
+// is used to update the data but also creates the data if it is not already there
+// to do task
+app.put("", () => {
+
+})
+
+app.delete("/product/:id", (req, res) => {
+  const {id} = req.params;
+  products = products.filter((product) => {
+    return product.id !== id
+  })
+  res.send("deleted");
+})
 
 module.exports = { app };
+
+
+const req = {
+  name: "abhinav",
+  address: "india"
+}
+
+const {role} = req
