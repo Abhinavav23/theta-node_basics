6b778d53788b163498236d4b6b5e6212650bf36a
Abhinav <abhinav@AVs-Mac.local>
2023-12-08 23:34:26 +0530

    [8 dec 2023] more schema validation, authentication, JWT

--- authetication/src/models/userModel.js
+++ authetication/src/models/userModel.js
@@ -1,23 +1,48 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: String,
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    mobile: Number
-})
+  firstName: {
+    type: String,
+    required: true,
+    minLength: [3, "please enter at least three characters"],
+    maxLength: 10,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 10,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 6,
+    maxLength: 25,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+    // validate: {
+    //   validator: function (value) {
+    //     console.log("value", value);
+    //      return true means validation success and vice versa
+    //     return /\d{3}-\d{3}-\d{4}/.test(value);
+    //   },
+    //   message: (props) =>
+    //     `${props.value} is not a valid password please try another`,
+    // },
+  },
+  mobile: {
+    type: String,
+    minLength: 10,
+  },
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"]
+  }
+});
 
 const User = model("users", UserSchema);
 
-module.exports = User
-
+module.exports = User;
